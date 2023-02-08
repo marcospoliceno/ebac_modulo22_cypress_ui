4eb38c5be9408f0131fee900525a1017e32bd12a
Marcos Policeno <marcospoliceno@gmail.com>
2023-02-08 20:40:54 -0300

    exercicio 23

--- cypress/support/pages/checkout.page.js
+++ cypress/support/pages/checkout.page.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
 export const checkoutPage = {
-    get orderNumber() { return cy.get(`.woocommerce-order-overview__order`) },
-    get pageSubTitle() { return cy.get('.woocommerce-notice') }
-}
+  get orderNumber() {
+    return cy.get(`.woocommerce-order-overview__order`);
+  },
+  get pageSubTitle() {
+    return cy.get(".woocommerce-notice");
+  },
+};
